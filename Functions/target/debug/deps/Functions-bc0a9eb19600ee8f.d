921d6f6c310f149e4f22f0bab7dba9467e7c4cfa
Heramb4270 <heramb.bhoodhar@gmail.com>
2023-06-11 17:46:49 +0530

    Functions

---
+++ Functions/target/debug/deps/Functions-bc0a9eb19600ee8f.d
@@ -0,0 +1,5 @@
+/home/heramb/Rust Learning/Functions/target/debug/deps/Functions-bc0a9eb19600ee8f.rmeta: src/main.rs
+
+/home/heramb/Rust Learning/Functions/target/debug/deps/Functions-bc0a9eb19600ee8f.d: src/main.rs
+
+src/main.rs:
---
+++ Functions/target/debug/deps/Functions-d4a10519c3ae14bd.d
@@ -0,0 +1,5 @@
+/home/heramb/Rust Learning/Functions/target/debug/deps/Functions-d4a10519c3ae14bd.rmeta: src/main.rs
+
+/home/heramb/Rust Learning/Functions/target/debug/deps/Functions-d4a10519c3ae14bd.d: src/main.rs
+
+src/main.rs:
