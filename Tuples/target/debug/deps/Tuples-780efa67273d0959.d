28a331c737ac36e4e74d00d5047812460e4fad24
Heramb4270 <heramb.bhoodhar@gmail.com>
2023-06-11 17:47:43 +0530

    Tuples

---
+++ Tuples/target/debug/deps/Tuples-780efa67273d0959.d
@@ -0,0 +1,5 @@
+/home/heramb/Rust Learning/Tuples/target/debug/deps/Tuples-780efa67273d0959.rmeta: src/main.rs
+
+/home/heramb/Rust Learning/Tuples/target/debug/deps/Tuples-780efa67273d0959.d: src/main.rs
+
+src/main.rs:
---
+++ Tuples/target/debug/deps/Tuples-e6f6ff912d8754c1.d
@@ -0,0 +1,5 @@
+/home/heramb/Rust Learning/Tuples/target/debug/deps/Tuples-e6f6ff912d8754c1.rmeta: src/main.rs
+
+/home/heramb/Rust Learning/Tuples/target/debug/deps/Tuples-e6f6ff912d8754c1.d: src/main.rs
+
+src/main.rs:
